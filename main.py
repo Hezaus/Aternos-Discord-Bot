cf25a5dae0eca4ced943b9551368edbf71f5ec10
unknown <ongmeo3316@gmail.com>
2022-09-20 16:25:51 +0700

    aaa done easy

--- main.py
+++ main.py
@@ -1,32 +1,52 @@
 import discord
+import asyncio
 from discord.ext import commands
 
 import os
 from dotenv import load_dotenv
 
+from python_aternos import Client, atwss
+
 load_dotenv()
 
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print('Logged on as', self.user)
+#Env stuff
+TOKEN = os.getenv('TOKEN')
+USER = os.getenv('USER')
+PASS = os.getenv('PASS')
 
-    async def on_message(self, message):
-        # don't respond to ourselves
-        if message.author == self.user:
-            return
+#Aternos stuff
+aternos = Client.from_credentials(USER, PASS)
 
-        if message.content == 'ping':
-            await message.channel.send('pong')
+servs = aternos.list_servers()[0]
 
+socket = servs.wss()
+
+#Discord stuff
 intents = discord.Intents.default()
 intents.message_content = True
-bot = MyClient(intents=intents)
-command = commands.Bot(command_prefix='>', intents=intents)
+bot = commands.Bot(command_prefix=commands.when_mentioned, intents=intents)
 
-@command.command()
-async def ping(ctx):
-    await ctx.send('safdasfsafsa')
+#bot events
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user} (ID: {bot.user.id})')
+    print('------')
 
-TOKEN = os.getenv('TOKEN')
+#socket stuff
+@socket.wssreceiver(atwss.Streams.status)
+async def status(info):
+    servs._info = info
 
+#commands
+@bot.command()
+async def StartServer(bot):
+    if servs._info.lang == 'online' :
+        await bot.send('Server has already start on themincraftpros.aternos.me:45328')
+    elif servs._info.lang == 'loading' :
+        await bot.send('Server is starting pls wait!')
+    else:
+        servs.start()
+        await bot.send('Server has started on themincraftpros.aternos.me:45328')
+
+asyncio.run(socket.connect())
 bot.run(TOKEN)
